4c6ddedac00379fc0e673cb7e12e9a4cb4941137
mk2296 <mk2296@srmist.edu.in>
2026-03-24 08:32:43 +0530

    UC1-ApplicationEntry

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,7 @@
+public class BookMyStayApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to hotel booking management system");
+        System.out.println("System Initialized Successfully");
+
+    }
+}
